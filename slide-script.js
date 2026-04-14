22181acb5f40b6233e045b8b5e4218be5ec24b72
Jane Khomenko <eva.khomenko.28@mail.ru>
2026-04-14 04:01:07 +0300

    Feat: add Yandex.Mertica counter
    This reverts commit f0b7aad53a4437622ce4115d95670df7106a652b.

--- slide-script.js
+++ slide-script.js
@@ -70,40 +70,6 @@ function closeMenu() {
 }
 
 // ===== INITIALIZATION ON PAGE LOAD =====
-// Yandex.Mertika counter
-function(m,e,t,r,i,k,a){
-    m[i] = m[i] || function(){(m[i].a=m[i].a||[]).push(arguments)};
-    m[i].l = 1 * new Date();
-    for (var j = 0; j < document.scripts.length; j++) {
-        if (document.scripts[j].src === r) {
-            return;
-        }
-    }
-    k = e.createElement(t)
-    a = e.getElementsByTagName(t)[0]
-    k.async = 1
-    k.src = r
-    a.parentNode.insertBefore(k,a)
-}(window,
-    document,
-    'script',
-    'https://mc.yandex.ru/metrika/tag.js?id=108527392',
-    'ym');
-
-ym(108527392,
-    'init',
-    {
-        ssr:true,
-        webvisor:true,
-        clickmap:true,
-        ecommerce:"dataLayer",
-        referrer: document.referrer,
-        url: location.href,
-        accurateTrackBounce:true,
-        trackLinks:true
-    }
-);
-
 // All event listeners and observers are set up here
 document.addEventListener('DOMContentLoaded', function () {
     try {
